09a64e41995f8076e9ae99230f392cea41bf2b3e
akshat1032 <akshat.30oct@gmail.com>
2020-12-12 15:50:53 +0530

    Modified : Service layer introduced to employee payroll app modified

---
+++ src/main/java/com/capgemini/employeepayrollapp/services/IEmployeePayrollService.java
@@ -0,0 +1,18 @@
+package com.capgemini.employeepayrollapp.services;
+
+import java.util.List;
+
+import com.capgemini.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.capgemini.employeepayrollapp.model.EmployeePayrollData;
+
+public interface IEmployeePayrollService {
+	List<EmployeePayrollData> getEmployeePayrollData();
+
+	EmployeePayrollData getEmployeePayrollDataById(int empId);
+
+	EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+
+	EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+
+	void deleteEmployeePayrollData(int empId);
+}
