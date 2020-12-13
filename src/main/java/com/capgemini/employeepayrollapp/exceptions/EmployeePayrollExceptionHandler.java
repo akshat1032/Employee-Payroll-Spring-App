559357815e829b1427daf76d29277714e791f2f0
akshat1032 <akshat.30oct@gmail.com>
2020-12-13 22:11:37 +0530

    Added : User friendly error response in case of failed validation added

---
+++ src/main/java/com/capgemini/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.capgemini.employeepayrollapp.exceptions;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.capgemini.employeepayrollapp.dto.ResponseDTO;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMsg = errorList.stream().map(objError -> objError.getDefaultMessage())
+				.collect(Collectors.toList());
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errMsg);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+}
