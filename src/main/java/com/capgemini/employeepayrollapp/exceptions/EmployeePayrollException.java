33789b898e3c61c5bb7f28716f4127ff0f686c47
akshat1032 <akshat.30oct@gmail.com>
2020-12-13 22:38:19 +0530

    Added : User friendly error response for employee id not found added

---
+++ src/main/java/com/capgemini/employeepayrollapp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,8 @@
+package com.capgemini.employeepayrollapp.exceptions;
+
+public class EmployeePayrollException extends RuntimeException {
+
+	public EmployeePayrollException(String message) {
+		super(message);
+	}
+}
