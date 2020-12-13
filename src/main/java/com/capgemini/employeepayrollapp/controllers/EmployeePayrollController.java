4ce855d1474ce0ee588b8b7488565684610a1d6b
akshat1032 <akshat.30oct@gmail.com>
2020-12-13 21:44:09 +0530

    Added : Name field validation added

--- src/main/java/com/capgemini/employeepayrollapp/controllers/EmployeePayrollController.java
+++ src/main/java/com/capgemini/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -2,6 +2,8 @@ package com.capgemini.employeepayrollapp.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +45,7 @@ public class EmployeePayrollController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeeDTO) {
+	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO employeeDTO) {
 		EmployeePayrollData empData = null;
 		empData = employeePayrollService.createEmployeePayrollData(employeeDTO);
 		ResponseDTO respDTO = new ResponseDTO("Get call for ID successful ", empData);
@@ -52,14 +54,14 @@ public class EmployeePayrollController {
 
 	@PutMapping("/update/{empId}")
 	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId,
-			@RequestBody EmployeePayrollDTO employeeDTO) {
+			@Valid @RequestBody EmployeePayrollDTO employeeDTO) {
 		EmployeePayrollData empData = null;
 		empData = employeePayrollService.updateEmployeePayrollData(empId, employeeDTO);
 		ResponseDTO respDTO = new ResponseDTO("Updated Employee Payroll Data Successfully ", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
-	@DeleteMapping("/delete")
+	@DeleteMapping("/delete/{empId}")
 	public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
 		employeePayrollService.deleteEmployeePayrollData(empId);
 		ResponseDTO respDTO = new ResponseDTO("Employee Payroll Data Deleted Successfully: ", empId);
